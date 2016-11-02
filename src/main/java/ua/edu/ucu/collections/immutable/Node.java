712d767f2b55a795509b1b28cda56876805c8793
paranyak_n <paranyak@ucu.edu.ua>
2016-11-02 17:56:54 +0200

    Initial commit

---
+++ src/main/java/ua/edu/ucu/collections/immutable/Node.java
@@ -0,0 +1,30 @@
+package ua.edu.ucu.collections.immutable;
+
+import java.util.Objects;
+
+/**
+ * Created by cs.ucu.edu.ua on 11/2/2016.
+ */
+public class Node {
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    private Object data;
+    private Node next;
+
+
+}
