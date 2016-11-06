0c038faef3652d8ddb00ab4c6aba3458a4c369b5
paranyak_n <paranyak@ucu.edu.ua>
2016-11-06 10:02:43 +0200

    Finished all methods in ImmutableLinkedList class

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
@@ -1,7 +1,7 @@
 package ua.edu.ucu.collections.immutable;
 
 import java.util.InputMismatchException;
-import java.util.Objects;
+
 
 public class ImmutableLinkedList implements ImmutableList{
 
@@ -28,7 +28,6 @@ public class ImmutableLinkedList implements ImmutableList{
         if(startN.getData() == null){
             newCurrent = startN.clone();
             newCurrent.setData(e);
-            sizeOfList ++;
         }else{
             newCurrent = startN.clone();
             Node tmp = newCurrent;
@@ -41,7 +40,6 @@ public class ImmutableLinkedList implements ImmutableList{
                 current = current.getNext();
             }
             tmp.setNext(element);
-            sizeOfList ++;
         }
 
         return new ImmutableLinkedList(newCurrent);
@@ -49,6 +47,7 @@ public class ImmutableLinkedList implements ImmutableList{
 
     @Override
     public ImmutableList add(int index, Object e) {
+        sizeOfList = size();
         if (index < 0 || index > sizeOfList || isEmpty()){
             throw new InputMismatchException("Index out of range");
         }
@@ -108,7 +107,6 @@ public class ImmutableLinkedList implements ImmutableList{
                 tmp.setNext(element);
                 tmp = element;
             }
-            sizeOfList = c.length;
         }else {
             newCurrent = startN.clone();
             Node tmp = newCurrent;
@@ -123,8 +121,6 @@ public class ImmutableLinkedList implements ImmutableList{
                 }
                 tmp.setNext(element);
                 tmp = element;
-                sizeOfList ++;
-
             }
         }
 
@@ -133,15 +129,54 @@ public class ImmutableLinkedList implements ImmutableList{
 
     @Override
     public ImmutableList addAll(int index, Object[] c) {
-        if (index < 0 || index > sizeOfList || isEmpty()){
-            throw new InputMismatchException("Index out of range");
+        Node newCurrent;
+        if(index == 0){
+            newCurrent = new Node(c[0]);
+            Node tmp = newCurrent;
+            for(int i = 1; i < c.length; i ++){
+                Node element = new Node(c[i]);
+                tmp.setNext(element);
+                tmp = element;
+            }
+
+            Node current_tmp = startN;
+            while(current_tmp != null){
+                Node n = current_tmp.clone();
+                tmp.setNext(n);
+                tmp = n;
+                current_tmp = current_tmp.getNext();
+            }
+        }else{
+            newCurrent = startN.clone();
+            Node tmp = newCurrent;
+            Node current = startN ;
+            for(int i = 0; i < index-1; i++){
+                Node n = current.getNext().clone();
+                tmp.setNext(n);
+                tmp = n;
+                current = current.getNext();
+            }
+            for(int i = 0; i < c.length; i ++){
+                Node element = new Node(c[i]);
+                tmp.setNext(element);
+                tmp = element;
+            }
+
+            Node current_tmp = current.getNext();
+            while(current_tmp != null){
+                Node n = current_tmp.clone();
+                tmp.setNext(n);
+                tmp = n;
+                current_tmp = current_tmp.getNext();
+            }
         }
 
-        return null;
+        return new ImmutableLinkedList(newCurrent);
     }
 
     @Override
     public Object get(int index) {
+        sizeOfList = size();
         if (index < 0 || index > sizeOfList || isEmpty()){
             throw new InputMismatchException("Index out of range");
         }
@@ -155,11 +190,47 @@ public class ImmutableLinkedList implements ImmutableList{
 
     @Override
     public ImmutableList remove(int index) {
-        return null;
+        sizeOfList = size();
+        if (index < 0 || index > sizeOfList || isEmpty()){
+            throw new InputMismatchException("Index out of range");
+        }
+        Node newCurrent;
+        if(index == 0){
+            newCurrent = startN.getNext();
+            Node tmp = newCurrent;
+            Node current_tmp = newCurrent.getNext();
+            while(current_tmp != null){
+                Node n = current_tmp.clone();
+                tmp.setNext(n);
+                tmp = n;
+                current_tmp = current_tmp.getNext();
+            }
+        }else{
+
+            newCurrent = startN.clone();
+            Node tmp = newCurrent;
+            Node current = startN ;
+            for(int i = 0; i < index-1; i++){
+                Node n = current.getNext().clone();
+                tmp.setNext(n);
+                tmp = n;
+                current = current.getNext();
+            }
+            Node current_tmp = current.getNext();
+            while(current_tmp.getNext() != null){
+                Node n = current_tmp.getNext().clone();
+                tmp.setNext(n);
+                tmp = n;
+                current_tmp = current_tmp.getNext();
+            }
+        }
+
+        return new ImmutableLinkedList(newCurrent);
     }
 
     @Override
     public ImmutableList set(int index, Object e) {
+        sizeOfList = size();
         if (index < 0 || index > sizeOfList || isEmpty()){
             throw new InputMismatchException("Index out of range");
         }
@@ -202,27 +273,54 @@ public class ImmutableLinkedList implements ImmutableList{
 
     @Override
     public int indexOf(Object e) {
-        return 0;
+        sizeOfList = size();
+        Node curNode = startN;
+        for(int i = 0; i < sizeOfList; i++){
+            if(curNode.getData() == e){
+                return i;
+            }else{
+                curNode = curNode.getNext();
+            }
+        }return -1;
     }
 
     @Override
     public int size() {
-        return 0;
+        int sizeOfL = 0;
+        Node curNode = startN;
+        while (curNode != null) {
+            sizeOfL++;
+            curNode = curNode.getNext();
+        }
+        return sizeOfL;
     }
 
     @Override
     public ImmutableList clear() {
-        return null;
+
+        return new ImmutableLinkedList();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        sizeOfList = size();
+        if(sizeOfList == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        sizeOfList = size();
+        Object [] newArray = new Object[sizeOfList];
+        Node current = startN;
+        for(int i = 0; i < sizeOfList; i ++){
+            newArray[i] = current.getData();
+            current = current.getNext();
+        }
+        return newArray;
     }
 
     public String toString(){
@@ -231,7 +329,7 @@ public class ImmutableLinkedList implements ImmutableList{
             presentation += startN.getData();
             Node start = startN;
             while(start.getNext() != null){
-                presentation += "- ";
+                presentation += " --> ";
                 start = start.getNext();
                 presentation += start.getData();
             }
@@ -244,7 +342,6 @@ public class ImmutableLinkedList implements ImmutableList{
 
 
     public static void main(String[] args){
-      //  Node a = new Node(1);
         ImmutableList m = new ImmutableLinkedList();
         System.out.println("m: "+ m);
         m = m.add(1);
@@ -254,14 +351,15 @@ public class ImmutableLinkedList implements ImmutableList{
         m_1 = m_1.add(4);
         m_1 = m_1.add(5);
         m_1 = m_1.add(6);
+        m_1 = m_1.add(7);
         m_1 = m_1.add(8);
         m_1 = m_1.add(9);
         m_1 = m_1.add(10);
         System.out.println("m: " + m);
         System.out.println("m_1(add to m): " +m_1);
-        ImmutableList m_2 = m_1.set(0, 7);
+        ImmutableList m_2 = m_1.set(0, 100);
         System.out.println("m: " +m);
-        System.out.println("m_2(set to m on 0 index 7): " +m_2);
+        System.out.println("m_2(set to m on 0 index 100): " +m_2);
 
         ImmutableList m_3 = m_1;
         System.out.println("m_3(= m_1): " +m_3);
@@ -283,5 +381,28 @@ public class ImmutableLinkedList implements ImmutableList{
         System.out.println("m_1(element on 2 index): " +m_1.get(2));
         System.out.println("m_5(element on 10 index): " + m_5.get(10));
 
+        System.out.println("m_6 size: "+ m_6.size());
+
+        ImmutableList m_7 = m_1.clear();
+        System.out.println("m_7(m_1.clear()): "+m_7);
+        System.out.println("m_1: "+m_1);
+
+        ImmutableList m_8 = m_1.remove(2);
+        System.out.println("m_8 (m_1 remove el on 2 index): "+ m_8);
+
+        System.out.println(m_1 + "  <-- m_1 index of element 3: "+m_1.indexOf(3));
+        System.out.println(m_1 + "  <-- m_1 index of element 11: "+m_1.indexOf(11));
+
+        System.out.println("m_1 is empty?  "+ m_1.isEmpty());
+
+        Integer[] c1 = {100, 200, 300, 400};
+        ImmutableList m_9 = m_1.addAll(2, c1);
+        System.out.println("m_9: "+m_9);
+
+        Integer [] c1_0 = {100, 200, 300, 400};
+        ImmutableList m_10 = m_1.addAll(0, c1_0);
+        System.out.println("m_10: "+ m_10);
+
+        System.out.println(m_1.toArray());
     }
 }
