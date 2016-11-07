af3b105a359da1677b88af8f6d4781474d063095
paranyak_n <paranyak@ucu.edu.ua>
2016-11-07 07:36:20 +0200

    Added tests for ImmutableArrayList and changed some methods in ImmutableLinkedList

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
@@ -1,6 +1,7 @@
 package ua.edu.ucu.collections.immutable;
 
 
+import java.util.InputMismatchException;
 import java.util.Objects;
 
 public class ImmutableArrayList implements ImmutableList{
@@ -22,9 +23,12 @@ public class ImmutableArrayList implements ImmutableList{
 
     @Override
     public ImmutableList add(int index, Object e) {
+        if (index < 0 || index > myList.length){
+            throw new InputMismatchException("Index out of range");
+        }
         int sizeOfFirstArray = myList.length;
         Object [] arrayNew1 = new Object[sizeOfFirstArray + 1];
-        System.arraycopy(myList, 0, arrayNew1, 0, index+1);
+        System.arraycopy(myList, 0, arrayNew1, 0, index);
         arrayNew1[index] = e;
         System.arraycopy(myList, index, arrayNew1, index+1, sizeOfFirstArray - index);
         return new ImmutableArrayList(arrayNew1);
@@ -42,6 +46,9 @@ public class ImmutableArrayList implements ImmutableList{
 
     @Override
     public ImmutableList addAll(int index, Object[] c) {
+        if (index < 0 || index > myList.length){
+            throw new InputMismatchException("Index out of range");
+        }
         int sizeOfFirstArray = myList.length;
         Object [] arrayNew1 = new Object[sizeOfFirstArray + c.length];
         System.arraycopy(myList, 0, arrayNew1, 0, index);
@@ -53,11 +60,18 @@ public class ImmutableArrayList implements ImmutableList{
 
     @Override
     public Object get(int index) {
-       return myList[index];
+        if (index < 0 || index > myList.length){
+            throw new InputMismatchException("Index out of range");
+        }
+        return myList[index];
+
     }
 
     @Override
     public ImmutableList remove(int index) {
+        if (index < 0 || index > myList.length){
+            throw new InputMismatchException("Index out of range");
+        }
         int sizeOfFirstArray = myList.length;
         Object [] arrayNew1 = new Object[sizeOfFirstArray - 1];
         System.arraycopy(myList, 0, arrayNew1, 0, index);
@@ -67,6 +81,9 @@ public class ImmutableArrayList implements ImmutableList{
 
     @Override
     public ImmutableList set(int index, Object e) {
+        if (index < 0 || index > myList.length){
+            throw new InputMismatchException("Index out of range");
+        }
         Object [] arrayNew1 = new Object[myList.length];
         System.arraycopy(myList, 0, arrayNew1, 0, myList.length);
         arrayNew1[index] = e;
@@ -109,46 +126,18 @@ public class ImmutableArrayList implements ImmutableList{
 
     public String toString(){
         String presentation = " ";
-        for(int i = 0; i < myList.length; i ++){
-            presentation += myList[i];
-            presentation += " ";
+
+        if(myList.length == 0){
+            presentation = "is empty";
+        }else{
+            for(int i = 0; i < myList.length-1; i ++){
+                presentation += myList[i];
+                presentation += ",";
+            }
+            presentation += myList[myList.length-1];
         }
+
         return presentation;
     }
-    public static void main(String [] args){
-        ImmutableList a = new ImmutableArrayList();
-        a = a.add(1);
-        a = a.add(2);
-        a = a.add(3);
-        a = a.add(4);
-        a = a.add(5);
-        a = a.add(6);
-        a = a.add(7);
-        a = a.add(8);
-        a = a.add(9);
-        a = a.add(10);
-        System.out.println("a: "+ a);
 
-        ImmutableList a_1 = a.add(11);
-        System.out.println("a_1(add to a 11): "+a_1);
-
-        ImmutableList a_2 = a.add(4, 100);
-        System.out.println("a_2(add to a on 4 index 100): "+ a_2);
-
-        Integer [] c = {11, 12, 13};
-        ImmutableList b = a.addAll(c);
-        System.out.println("b: "+ b);
-
-        Integer [] c1 = {15, 16, 17};
-        ImmutableList b1 = a.addAll(0, c1);
-        System.out.println("b1: "+ b1);
-
-        System.out.println(b1.get(4));
-
-        ImmutableList c_1 = a.remove(1);
-        System.out.println("c_1:  "+c_1);
-
-        ImmutableList d = a.set(5, 100);
-        System.out.println("d: " + d);
-    }
 }
--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
@@ -1,12 +1,241 @@
 package ua.edu.ucu.collections.immutable;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
 import static org.junit.Assert.*;
 
 public class ImmutableArrayListTest {
-    
+
     @Test
-    public void testSomeMethod() {
+    public void testMethodAdd() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(1);
+        m = m.add(2);
+        m = m.add(3);
+        ImmutableList actualResult = m;
+        assertEquals(" 1,2,3", actualResult.toString());
     }
-    
+
+    @Test(expected = InputMismatchException.class)
+    public void testMethodAddIndex2() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(2, 9);
+    }
+
+    @Test
+    public void testMethodAddIndex() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(1);
+        ImmutableList second = m.add(2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        ImmutableList actualResult = second;
+        assertEquals(" 1,2,4,3", actualResult.toString());
+    }
+    @Test
+    public void testMethodAddIndex3() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        ImmutableList actualResult = second;
+        assertEquals(" 1,2,4,5,3", actualResult.toString());
+    }
+
+    @Test
+    public void testMethodAddAll() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        Integer [] c = {6, 7, 8};
+        second = second.addAll(c);
+        ImmutableList actualResult = second;
+        assertEquals(" 1,2,4,5,3,6,7,8", actualResult.toString());
+    }
+
+    @Test
+    public void testMethodAddAllStart() {
+        ImmutableList second = new ImmutableArrayList();
+
+        Integer [] c = {6, 7, 8};
+        second = second.addAll(c);
+        ImmutableList actualResult = second;
+        assertEquals(" 6,7,8", actualResult.toString());
+    }
+
+    @Test
+    public void testMethodAddAllIndex() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        Integer [] c = {6, 7, 8};
+        second = second.addAll(1,c);
+        ImmutableList actualResult = second;
+        assertEquals(" 1,6,7,8,2,4,5,3", actualResult.toString());
+    }
+    @Test(expected = InputMismatchException.class)
+    public void testMethodAddAllE() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        Integer [] c = {6, 7, 8};
+        second = second.addAll(10,c);
+
+    }
+
+    @Test
+    public void testMethodGet() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        Integer [] c = {6, 7, 8};
+        second = second.addAll(c);
+        Object actualResult = second.get(0);
+        assertEquals(1, actualResult);
+    }
+
+    @Test(expected = InputMismatchException.class)
+    public void testMethodGetE() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second.get(10);
+    }
+    @Test
+    public void testMethodRemove() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        ImmutableList actualResult = second.remove(2);
+        assertEquals(" 1,2,5,3", actualResult.toString());
+    }
+
+    @Test(expected = InputMismatchException.class)
+    public void testMethodRemoveE() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        second.remove(12);
+    }
+
+    @Test
+    public void testMethodSet() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        ImmutableList actualResult = second.set(2,8);
+        assertEquals(" 1,2,8,5,3", actualResult.toString());
+    }
+
+    @Test(expected = InputMismatchException.class)
+    public void testMethodSetE() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        second.set(13,7777);
+    }
+
+    @Test
+    public void testMethodIndexOf() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        Object actualResult = second.indexOf(4);
+        assertEquals(2, actualResult);
+    }
+
+    @Test
+    public void testMethodIndexOfE() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        Object actualResult = second.indexOf(14);
+        assertEquals(-1, actualResult);
+    }
+
+    @Test
+    public void testMethodSize() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        int actualResult = second.size();
+        assertEquals(5, actualResult);
+    }
+
+    @Test
+    public void testMethodClear() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        ImmutableList actualResult = second.clear();
+        assertEquals("is empty", actualResult.toString());
+    }
+
+    @Test
+    public void testMethodEmpty() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        second = second.clear();
+        boolean actualResult = second.isEmpty();
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void testMethodToArray() {
+        ImmutableList m = new ImmutableArrayList();
+        m = m.add(0, 1);
+        ImmutableList second = m.add(1, 2);
+        second = second.add(2, 3);
+        second = second.add(2, 4);
+        second = second.add(3, 5);
+        Object [] actualResult = second.toArray();
+        Object [] expected = {1, 2, 4, 5, 3};
+        assertEquals(Arrays.toString(expected), Arrays.toString(actualResult));
+    }
 }
