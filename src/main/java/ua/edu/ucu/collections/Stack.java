16035fb6e3bda1fea73abef6f1ff6d5a9a6a7576
paranyak_n <paranyak@ucu.edu.ua>
2016-11-07 17:44:49 +0200

    Added class StackTest

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -19,5 +19,8 @@ public class Stack {
 
         myList = (ImmutableLinkedList) myList.add(e);
     }
-    
+
+    public  String toString(){
+        return myList.toString();
+    }
 }
--- src/test/java/ua/edu/ucu/collections/StackTest.java
+++ src/test/java/ua/edu/ucu/collections/StackTest.java
@@ -7,6 +7,33 @@ public class StackTest {
     
     @Test
     public void testSomeMethod() {
+        Stack st = new Stack();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        Stack actualResult = st;
+        assertEquals(" 1 --> 2 --> 3", actualResult.toString());
     }
-    
+
+
+    @Test
+    public void testSomeMethod2() {
+        Stack st = new Stack();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        Object actualResult = st.peek();
+        assertEquals(1, actualResult);
+    }
+
+    @Test
+    public void testSomeMethod3() {
+        Stack st = new Stack();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        Object actualResult = st.pop();
+        assertEquals(1, actualResult);
+    }
+
 }
